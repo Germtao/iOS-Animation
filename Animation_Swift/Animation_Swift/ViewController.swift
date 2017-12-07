2c62a0a9479b0eb436caac80332272acae37f7a9
Germtao <TAO@MbPro.local>
2017-12-07 15:17:34 +0800

    设置根控制器&初始化vc子控件

--- Animation_Swift/Animation_Swift/ViewController.swift
+++ Animation_Swift/Animation_Swift/ViewController.swift
@@ -2,24 +2,131 @@
 //  ViewController.swift
 //  Animation_Swift
 //
-//  Created by TAO on 2017/12/7.
-//  Copyright © 2017年 ShaggyT. All rights reserved.
+//  Created by TAO on 2017/7/25.
+//  Copyright © 2017年 Germtao. All rights reserved.
 //
 
 import UIKit
 
+var titles: [String] = []
+
 class ViewController: UIViewController {
+    
+    var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "动画效果"
+        navigationController?.navigationBar.barTintColor = UIColor.cyan
+        
+        initData()
+        
+        initSubViews()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    // MARK - Helper Func
+    func setMessageLabel(_ messageLabel: UILabel, frame: CGRect, text: String, textColor: UIColor, numberOfLines: Int, textAlignment: NSTextAlignment, font: UIFont) {
+        
+        messageLabel.frame = frame
+        messageLabel.text = text
+        messageLabel.textColor = textColor
+        messageLabel.numberOfLines = numberOfLines
+        messageLabel.textAlignment = textAlignment
+        messageLabel.font = font
+        messageLabel.sizeToFit()
+    }
+    
+    func setCellWithData(_ cell: UITableViewCell, title: String) {
+        
+        cell.textLabel?.text = title;
+    }
+    
+    // MARK - Private Method
+    private func initData() {
+        
+        titles = ["基础动画", "关键帧动画", "组动画", "过渡动画", "仿射变换", "综合案例", "文字动画", "碎片动画"];
     }
 
+    private func initSubViews() {
+        
+        let frame: CGRect = CGRect.init(x: 0, y: 20, width: kSCREEN_WIDTH, height: kSCREEN_HEIGHT - 20)
+        tableView = UITableView.init(frame: frame, style: UITableViewStyle.plain)
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        tableView.contentInset = UIEdgeInsetsMake(10, 0, 10, 0)
+        self.view.addSubview(tableView)
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if titles.count != 0 {
+            return titles.count
+        } else {
+            let messageLabel: UILabel = UILabel()
+            
+            setMessageLabel(messageLabel, frame: CGRect.init(x: 0, y: 20, width: kSCREEN_WIDTH, height: kSCREEN_HEIGHT - 20), text: "没有数据", textColor: UIColor.black, numberOfLines: 0, textAlignment: NSTextAlignment.center, font: UIFont.init(name: "Palatino-Italic", size: 20)!)
+            
+            self.tableView.backgroundView = messageLabel
+            self.tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+            
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellID: String = "cell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        
+        if cell == nil {
+            cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: cellID)
+        }
+        
+        setCellWithData(cell!, title: titles[(indexPath as NSIndexPath).row])
+        
+        return cell!
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+//        var vc = BaseViewController()
+//
+//        switch indexPath.row {
+//        case 0:
+//            vc = BasicAnimationController()
+//        case 1:
+//            vc = KeyFrameAnimationController()
+//        case 2:
+//            vc = GroupAnimationController()
+//        case 3:
+//            vc = TransitionAnimationController()
+//        case 4:
+//            vc = AffineTransformController()
+//        case 5:
+//            vc = ComprehensiveCaseViewController()
+//        case 6:
+//            vc = TTTextEffectsViewController()
+//        default:
+//            break
+//        }
+        
+//        vc.view.backgroundColor = UIColor.white
+//        vc.title = titles[indexPath.row]
+//        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
